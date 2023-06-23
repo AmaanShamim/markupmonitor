f8b56e7ec2d94575ac1ff20809a8d2a452c1ab31
Amaan <amaans1912@gmail.com>
2023-06-23 15:53:17 +0530

    completed structuring and anded icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import './App.css';
+import React, { useState } from "react";
+import InputPreviewer from './Components/InputPreviewer';
+import Navbar from "./Components/Navbar";
 
 
 function App() {
+  const [mode, setmode] = useState("light"); // Whether dark mode is enabled or not.
+  const toggleMode = () => {
+    if (mode === "light") {
+      setmode("dark");
+      document.body.style.backgroundColor = "#2e2948";
+      document.body.style.color = "White";
+    } else {
+      setmode("light");
+      document.body.style.backgroundColor = "White";
+      document.body.style.color = "Black";
+    }
+  };
   return (
     <>
-     <h1>Hello World</h1>
+     <Navbar mode={mode} toggleMode={toggleMode} />
+     <InputPreviewer />
     </>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Navbar(props) {
+  return (
+    <>
+      <div className="container d-flex justify-content-between my-4">
+        <h1 style={{
+            fontFamily: "Library"
+        }}>Markup Monitor</h1>
+        <div style={{
+            display: "flex",
+            alignItems: "center"
+        }}>
+          {props.mode==="dark"?<i onClick={props.toggleMode} className="fa-solid fa-sun fa-2xl"></i>:<i onClick={props.toggleMode} className="fa-solid fa-moon fa-2xl"></i>}
+        </div>
+      </div>
+    </>
+  )
+}
